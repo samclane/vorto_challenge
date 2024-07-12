1e2d15afbf30240ff6956622b2c3751904d10b2a
Sawyer McLane <samclane@gmail.com>
2024-07-12 15:16:48 -0600

    more readability

--- mySolution.py
+++ mySolution.py
@@ -1,7 +1,7 @@
 import csv
 from itertools import permutations
 import math
-from typing import Any
+from typing import Any, List, Dict
 import argparse
 from functools import lru_cache
 
@@ -18,7 +18,7 @@ class Point:
         return "Point{}".format(str(self))
 
     @classmethod
-    def from_str(cls, s: str):
+    def from_str(cls, s: str) -> "Point":
         x, y = s.strip("()").split(",")
         return cls(float(x), float(y))
 
@@ -28,9 +28,9 @@ class Point:
 
 @lru_cache(None)
 def distance_between_points(p1: Point, p2: Point) -> float:
-    xDiff = p1.x - p2.x
-    yDiff = p1.y - p2.y
-    return math.sqrt(xDiff * xDiff + yDiff * yDiff)
+    xd = p1.x - p2.x
+    yd = p1.y - p2.y
+    return math.sqrt(xd * xd + yd * yd)
 
 
 class Load:
@@ -47,7 +47,7 @@ class Load:
 
 
 class Route:
-    def __init__(self, loads: list[Load]):
+    def __init__(self, loads: List[Load]):
         self.loads = loads
         self.time = self.calculate_time()
 
@@ -58,7 +58,8 @@ class Route:
             t += pos.distance(l.pickup)
             t += l.pickup.distance(l.dropoff)
             pos = l.dropoff
-        t += pos.distance(DEPOT)
+        if pos != DEPOT:
+            t += pos.distance(DEPOT)
         return t
 
     @property
@@ -73,7 +74,7 @@ class Route:
 
 
 class Solution:
-    def __init__(self, routes: list[Route]):
+    def __init__(self, routes: List[Route]):
         self.routes = routes
         self.is_valid: bool = all(r.is_valid for r in self.routes)
         self.cost: float = DRIVER_COST * self.num_drivers + sum(
@@ -88,13 +89,13 @@ class Solution:
         return "\n".join(map(str, self.routes))
 
     def __repr__(self) -> str:
-        return "Solution({}, {}, {})".format(self.routes, self.cost, self.num_drivers)
+        return f"Solution({self.routes}, {self.cost}, {self.num_drivers})"
 
     def __getitem__(self, i) -> Route:
         return self.routes[i]
 
 
-def read_file(path: str) -> list[dict[str, Any]]:
+def read_file(path: str) -> List[Dict[str, Any]]:
     with open(path, "r") as f:
         reader = csv.DictReader(f, delimiter=" ")
         return list(reader)
@@ -140,10 +141,10 @@ def solve(filename: str) -> Solution:
         for l in read_file(filename)
     ]
 
-    sl = permute_loads(loads)
-    solutions = [s for s in map(Solution, sl) if s.is_valid]
-    solutions = sorted(solutions, key=lambda s: s.cost, reverse=False)
-    return solutions[0]
+    return min(
+        [s for s in map(Solution, permute_loads(loads)) if s.is_valid],
+        key=lambda s: s.cost,
+    )
 
 
 if __name__ == "__main__":
